e36a7fc24ac8b40b6ae26cd74a722c2bbd0dc78e
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2022-11-17 09:18:35 +0700

    feat: create dictionary with IO methods and search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import Object.Dictionary;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dictionary myDict = new Dictionary();
+        int count = 1;
+
+        myDict.readFromFile("test.txt");
+        System.out.println(myDict.search("Æ"));
+        myDict.writeToFile("test.txt");
     }
 }
--- src/Object/Dictionary.java
+++ src/Object/Dictionary.java
@@ -1,7 +1,151 @@
 package Object;
 
-import java.util.HashMap;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.*;
 
 public class Dictionary {
-    private HashMap<String, String> dictionary;
+    public HashMap<String, String> dictionary;
+
+    Scanner scanner;
+
+    public Dictionary() {
+        dictionary = new HashMap<>();
+    }
+
+    public boolean add(String word, String meaning) {
+        if (word.isEmpty() || meaning.isEmpty() || dictionary.containsKey(word)) {
+            return false;
+        }
+
+        dictionary.put(word.toUpperCase(), meaning);
+        return true;
+    }
+
+    public boolean replace(String word, String meaning) {
+        if (word.isEmpty() || meaning.isEmpty() || !dictionary.containsKey(word)) {
+            return false;
+        }
+
+        dictionary.replace(word, meaning);
+        return true;
+    }
+
+    public boolean addNewMeaning(String word, String meaning) {
+        if (word.isEmpty() || meaning.isEmpty() || !dictionary.containsKey(word)) {
+            return false;
+        }
+
+        String newMeaning = dictionary.get(word) + "&&" + meaning;
+
+        dictionary.put(word.toUpperCase(), newMeaning);
+        return true;
+    }
+
+    public boolean delete(String word) {
+        if (!dictionary.containsKey(word.toUpperCase())) {
+            return false;
+        }
+
+        dictionary.remove(word);
+        return true;
+    }
+
+    public String search(String word) {
+        if (!dictionary.containsKey(word.toUpperCase())) {
+            return null;
+        }
+
+        return dictionary.get(word.toUpperCase());
+    }
+
+    public int getSize() {
+        return dictionary.size();
+    }
+
+    public void readFromFile(String filename) {
+        try {
+            scanner = new Scanner(new File(filename));
+            scanner.nextLine();
+            String trace = null;
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.isBlank()) {
+                    continue;
+                }
+
+                int breakPoint = line.indexOf("`");
+
+                if (breakPoint == -1) {
+                    if (trace != null) {
+                        String newMeaning = dictionary.get(trace);
+                        newMeaning += "&&" + line;
+                        dictionary.replace(trace, newMeaning);
+                    }
+
+                    continue;
+                }
+
+                String word = line.substring(0, breakPoint);
+                String meaning = line.substring(breakPoint + 1);
+                trace = word;
+
+                if (dictionary.containsKey(word)) {
+                    if (dictionary.get(word) == meaning) {
+                        continue;
+                    }
+
+                    String newMeaning = dictionary.get(word) + "&&" + line;
+                    dictionary.replace(word, newMeaning);
+                    continue;
+                }
+
+                add(word, meaning);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Read from file "+filename+" successfully!");
+            scanner.close();
+        }
+    }
+
+    public void writeToFile(String filename) {
+        try {
+            TreeMap<String, String> treeMap = new TreeMap<>(dictionary);
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write("Slag`Meaning\n");
+
+            for (String word : treeMap.keySet()) {
+                String meaning = treeMap.get(word);
+                String anotherMeaning = null;
+
+                if (!meaning.contains("&&")) {
+                    writer.write(word+"`"+meaning+"\n");
+                    writer.flush();
+                    continue;
+                }
+
+                writer.write(word+"`"+meaning.substring(0, meaning.indexOf("&"))+"\n");
+                meaning = meaning.substring(meaning.indexOf("&") + 2);
+
+                while (meaning.contains("&&")) {
+                    anotherMeaning = meaning.substring(0, meaning.indexOf("&"));
+                    meaning = meaning.substring(meaning.indexOf("&") + 2);
+                    writer.write(anotherMeaning+"\n");
+                }
+                writer.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Write to file "+filename+" successfully!");
+        }
+    }
+
 }
